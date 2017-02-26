e1ab0a80fe8d4bf1ec4b095500e22c21a5cf3c6b
ywq <1454072136@qq.com>
2017-02-26 18:43:21 +0800

    为备忘录添加SQLite数据库持久支持以及用于重要备忘的新颜色

--- app/src/main/java/com/apress/gerber/reminders1/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders1/RemindersActivity.java
@@ -1,30 +1,60 @@
 package com.apress.gerber.reminders1;
 
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.List;
-
-public class RemindersActivity extends AppCompatActivity {
+public class RemindersActivity extends ActionBarActivity {
     private ListView mListView;
+    private RemindersDbAdapter mDbAdapter;
+    private RemindersSimpleCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);//溢出菜单
         setSupportActionBar(toolbar);
         mListView = (ListView) findViewById(R.id.reminders_list_view);
+
+        mListView.setDivider(null);
+        mDbAdapter = new RemindersDbAdapter(this);
+        mDbAdapter.open();
+
+        Cursor cursor = mDbAdapter.fetchAllReminders();
+
+        //from columns defined in the db
+        String[] from = new String[]{RemindersDbAdapter.COL_CONTENT};
+        //to the ids of views in the layout
+        int[] to = new int[]{R.id.row_text};
+        mCursorAdapter = new RemindersSimpleCursorAdapter(
+                //context
+                RemindersActivity.this,
+                //the layout of the row
+                R.layout.reminders_row,
+                //cursor
+                cursor,
+                //from columns defined in the db
+                from,
+                //to the ids of views in the layout
+                to,
+                //flag - not used
+                0);
+        //the cursorAdapter (controller) is now updating the listView (view)
+        //with data from the db (model)
+        mListView.setAdapter(mCursorAdapter);
+    }
+    //Abbrevisted for brevity
+}
+    /****************
+
         // The arrayAdatper is the controller in our
         // moder-view-contorller relationship. (controller)
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
@@ -62,3 +92,4 @@ public class RemindersActivity extends AppCompatActivity {
         }
     }
 }
+     *********************/
